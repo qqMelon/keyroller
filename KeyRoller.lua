59834ff7af075c105331b0749305a190cd31f1c1
Mumu <itsmumu67@gmail.com>
2025-05-03 18:41:19 +0200

    fix score missing

--- KeyRoller.lua
+++ KeyRoller.lua
@@ -96,7 +96,7 @@ local function GetColorForScore(score)
         return "|cffff00ff" -- pink
     elseif score >= 2500 and score < 3000 then
         return "|cffa335ee" -- purple
-    elseif score >= 2000 and < 2500 then
+    elseif score >= 2000 and score < 2500 then
         return "|cff0070dd" -- blue
     elseif score >= 1000 and score < 2000 then
         return "|cff1eff00" -- green
