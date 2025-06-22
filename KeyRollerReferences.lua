329079140f4a673d303df81d2e4f684ad6f934bd
ItsMumu <79212720+ItsMumu@users.noreply.github.com>
2025-06-22 20:04:14 +0200

    Manage locale for dung names (#52)
    * adding dung name translate for french game client
    
    * fix syntax
    
    ---------
    
    Co-authored-by: qqMelon <urie.zahard@gmail.com>

---
+++ KeyRollerReferences.lua
@@ -0,0 +1,30 @@
+local addonName, addonTable = ...
+addonTable.constFRLocale = "frFR"
+addonTable.constENLocale = "enUS"
+
+--- locale FR ---
+
+addonTable.dungListFR = {
+[1] = "Faille de Flamme-Noire",
+[2] = "Prieuré de la Flamme sacrée",
+[3] = "La colonie",
+[4] = "Théâtre de la Souffrance",
+[5] = "Le Filon",
+[6] = "Opération Mécagone - L’atelier",
+[7] = "Opération Vannes ouvertes",
+[8] = "Hydromellerie de Brassecendre"
+}
+
+ --- DEFAULT locale EN ---
+ 
+addonTable.dungListEN = {
+[1] = "Darkflame Cleft", 
+[2] = "Priory of the Sacred Flame",
+[3] = "The Rookery",
+[4] = "Theater of Pain",
+[5] = "The MOTHERLODE!!",
+[6] = "Operation: Mechagon - Workshop",
+[7] = "Operation: Floodgate",
+[8] = "Cinderbrew Meadery"
+}
+	
