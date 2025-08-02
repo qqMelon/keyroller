b10a062b778532764f13c28a5c0db8686a98134c
ItsMumu <79212720+ItsMumu@users.noreply.github.com>
2025-08-02 04:27:08 +0200

    Update KeyRollerReferences.lua

--- KeyRollerReferences.lua
+++ KeyRollerReferences.lua
@@ -31,7 +31,6 @@ addonTable.dungListEN = {
 [5] = "The MOTHERLODE!!",
 [6] = "Operation: Mechagon - Workshop",
 [7] = "Operation: Floodgate",
-
 [8] = "Cinderbrew Meadery",
 [9] = "The Dawnbreaker",
 [10] = "Ara-Kara, City of Echoes",
