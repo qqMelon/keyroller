f922efb6b428986c66944b304aa3b05848c2c429
ItsMumu <79212720+ItsMumu@users.noreply.github.com>
2025-09-22 00:20:17 +0200

    Update KeyRollerReferences.lua

--- KeyRollerReferences.lua
+++ KeyRollerReferences.lua
@@ -42,7 +42,7 @@ dungBckPath = {
 	[2662] = "Interface\\lfgframe\\lfgicon-thedawnbreaker.blp", -- dawnbreaker
 	[2649] = "Interface\\lfgframe\\lfgicon-prioryofthesacredflames.blp", -- priory
 	[2773] = "Interface\\lfgframe\\lfgicon-waterworks.blp", -- floodgate
-=======
+}
 
 dungTPName = {
 	[2830] = "Aldani",
